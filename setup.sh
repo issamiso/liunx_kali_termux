27e7be50216f91646405db885c751d743d9d5b0e
issamiso <101994309+issamiso@users.noreply.github.com>
2022-12-13 23:18:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ pkg install python -y
 pkg install python2 -y
 pkg install python3 -y
 pkg install ruby -y
-wget https://raw.githubusercontent.com/issamiso/liunx_kali_termux/main/.iso.py 
-python3 .iso.py
+#wget https://raw.githubusercontent.com/issamiso/liunx_kali_termux/main/.iso.py 
+mv kali-setup.py $HOME
+cd $HOME;python3 kali-setup.py
